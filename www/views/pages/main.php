f134657673e4a47a1d9c6f4c3c96763ce33a58c3
Rafael Camps <rcamps@cifpfbmoll.eu>
2020-06-30 12:23:50 +0200

    Creados elementos página principal - close #14

---
+++ www/views/pages/main.php
@@ -0,0 +1,74 @@
+<div class="content container mt-5 bg-light">
+
+
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 pt-5 pb-3">
+        <div href="#" class="col mb-4">
+            <div class="card h-100">
+                <img src="./public/img/burger.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <a class="stretched-link text-decoration-none" href="#">
+                        <h5 class="card-title text-center">Nombre restaurante</h5>
+                    </a>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col mb-4">
+            <div class="card h-100">
+                <img src="./public/img/ensalada.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <a class="stretched-link text-decoration-none" href="#">
+                        <h5 class="card-title text-center">Nombre restaurante</h5>
+                    </a>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col mb-4">
+            <div class="card h-100">
+                <img src="./public/img/filete.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <a class="stretched-link text-decoration-none" href="#">
+                        <h5 class="card-title text-center">Nombre restaurante</h5>
+                    </a>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col mb-4">
+            <div class="card h-100">
+                <img src="./public/img/pescado.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <a class="stretched-link text-decoration-none" href="#">
+                        <h5 class="card-title text-center">Nombre restaurante</h5>
+                    </a>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col mb-4">
+            <div class="card h-100">
+                <img src="./public/img/sandwich.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <a class="stretched-link text-decoration-none" href="#">
+                        <h5 class="card-title text-center">Nombre restaurante</h5>
+                    </a>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col mb-4">
+            <div class="card h-100">
+                <img src="./public/img/tacos.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <a class="stretched-link text-decoration-none" href="#">
+                        <h5 class="card-title text-center">Nombre restaurante</h5>
+                    </a>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
